7e1de0ecf30bf4568ea238f799fd0d52048b7934
vinod <vinod.singh1@tothenew>
2020-02-18 16:26:50 +0530

    JAVA 1

--- que3.js
+++
@@ -1,8 +0,0 @@
-document.write('Find the area of circle');
-function areaCircle(){
-    document.write("<br>");
-    var radius = prompt('enter the radius of circle: ');
-    var areaCir = 3.14*radius*radius;
-    document.write(" The radius of circle is: ",areaCir);
-}
-areaCircle()
